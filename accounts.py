9b07c4504eaa93ae5701bf6599d1b3c6901ea9d8
Msyoki <msyokimutua@gmail.com>
2020-09-06 00:29:48 +0300

    add methods to generate random password and account list display

--- accounts.py
+++ accounts.py
@@ -1,3 +1,6 @@
+from random import choice
+import string
+
 class Account:
     """
     class that generates instances of users accounts
@@ -9,7 +12,7 @@ class Account:
     def __init__(self,profile_password, account_name, account_password):
         '''
         __init__ method to define the properties of a User object
-        Args:
+        Args:    
             profile_password : password of the user profile
             account_name : name of an account
             account_password : password for the account
@@ -32,4 +35,37 @@ class Account:
         """
 
         Account.account_list.remove(self)
+     
+    @classmethod   
+    def generate_password(cls):
+        """
+        Method that generates a random alphanumeric password
+        """
+
+        alphanum = string.ascii_uppercase + string.digits + string.ascii_lowercase
+        #string.ascii_uppercase/lowercase (String constants) - These values are not locale-dependent and will not change.
+         
+        password = ''.join( choice(alphanum) for num in range(8) )
+        # Create password
         
+        return password
+
+    @classmethod
+    def display_account(cls,password):
+        '''
+        Method that returns the account list
+        Args:
+            password : the user password
+        '''
+        profile_account_list = []
+
+        for account in cls.account_list:
+            if account.profile_password == password:
+                profile_account_list.append(account)
+
+        return profile_account_list
+
+        
+
+
+
