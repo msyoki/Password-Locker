c7474a6f99dd7238169cae80d0859b7226025eb2
Msyoki <msyokimutua@gmail.com>
2020-09-06 01:24:12 +0300

    run  test_find_account

--- profile_test.py
+++ profile_test.py
@@ -14,7 +14,7 @@ class TestProfile(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_profile = Profile("JohnDoe","Passlockjd2020#") # create profile object
+        self.new_profile = Profile("JohnDoe","JohnD2020#") # create profile object
 
 
     def test_init(self):
@@ -23,7 +23,7 @@ class TestProfile(unittest.TestCase):
         '''
 
         self.assertEqual(self.new_profile.profile_username,"JohnDoe")
-        self.assertEqual(self.new_profile.profile_password,"Passlockjd2020#")
+        self.assertEqual(self.new_profile.profile_password,"JohnD2020#")
         
     def test_save_profile(self):
         """
@@ -31,7 +31,24 @@ class TestProfile(unittest.TestCase):
         """
 
         self.new_profile.save_profile() # saving the new profile
-        self.assertEqual(len(Profile.profile_list),1)
+        self.assertEqual(len(Profile.profile_list),3)
+
+
+    def test_find_account(self):
+        '''
+        Test case to test if the Profile module is importing from accounts module
+        '''
+
+        # Save the new profile
+        self.new_profile.save_profile()
+
+        test_profile = Profile("LizDoe","LizD2020#")
+
+        test_profile.save_profile()
+
+        found_account = Profile.find_account("Gmail")
+
+        self.assertEqual( found_account, False )
 
 
 if __name__ == '__main__':
